d5ce9e0a2223cf2492892c19d4949c0b111f9a98
Gabriellyan Catheline <gabriellyan@pintu.co.id>
2023-09-02 01:07:39 +0700

    add images

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var rateLimit = require("express-rate-limit");
+var favicon = require('serve-favicon');
 
 var textRouter = require('./routes/search');
 var imageRouter = require('./routes/image');
@@ -18,9 +20,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/images/Counter.webp'));
+app.use(
+  rateLimit({
+    windowMs: 12 * 60 * 60 * 1000, // 12 hour duration in milliseconds
+    max: 100,
+    message: "You exceeded 100 requests in 24 hour limit!",
+    headers: true,
+  })
+);
 
 app.use('/image', imageRouter);
 app.use('/', textRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,35 @@
-var express = require('express');
-var router = express.Router();
-var Fuse = require('fuse.js')
+const express = require('express');
+const router = express.Router();
+const Fuse = require('fuse.js')
+const fs = require('fs');
+const axios = require('axios');
+const db = require('./../db/db.json')
+const images = require('./../db/images.json')
 
-const db = require('./db.json')
+const download_image = async (url, image_path) =>
+  axios({
+    url,
+    responseType: 'stream',
+  }).then(
+    response =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(image_path))
+          .on('finish', () => resolve())
+          .on('error', e => reject(e));
+      }),
+  );
 
-let idx = require('./name-index.json')
+const check_image = (konami_id) => {
+	let filePath =  __dirname + "/../public/images/cards/"+konami_id+".png"
+	let exists = fs.existsSync(filePath)
+	if (!exists) {
+		download_image(images[konami_id], filePath)
+	}
+	return exists
+}
+
+let idx = require('./../db/name-index.json')
 const nameIndex = Fuse.parseIndex(idx)
 const nameOptions = {
 	keys: [
@@ -14,9 +39,10 @@ const nameOptions = {
 
 const nameFuse = new Fuse(db, nameOptions, nameIndex)
 
-idx = require('./set-index.json')
+idx = require('./../db/set-index.json')
 const setIndex = Fuse.parseIndex(idx)
 const setOptions = {
+	useExtendedSearch: true,
 	keys: [
 		"card_sets",
 	]
@@ -24,17 +50,41 @@ const setOptions = {
 
 const setFuse = new Fuse(db, setOptions, setIndex)
 
+idx = require('./../db/id-index.json')
+const idIndex = Fuse.parseIndex(idx)
+const idOptions = {
+	useExtendedSearch: true,
+	keys: [
+		"konami_id",
+	]
+};
+
+const idFuse = new Fuse(db, idOptions, idIndex)
+
 router.get('/', function(req, res, next) {
   	res.render('search', { name: '? type here to search', result: [] });
 });
 
 router.get('/:name', function(req, res, next) {
+	let search = []
 	if(req.params.name.length == 10 &&  req.params.name[4] == '-') {
-		search = [setFuse.search(req.params.name)[0]]
+		search = setFuse.search('='+req.params.name)
 	} else {
 		search = nameFuse.search(req.params.name).slice(0, 10)
 	}
+
+	for(s of search) {
+		s.image_exists = check_image(s.item.konami_id)	
+	}
+
   	res.render('search', { name: req.params.name, result: search });
+});
+
+router.get('/card/:id', async function(req, res, next) {
+	search = idFuse.search('='+req.params.id)
+	search[0].image_exists = await check_image(search[0].item.konami_id)
+
+  	res.render('search', { name: req.params.name, result: [search[0]] });
 });
 
 module.exports = router;
